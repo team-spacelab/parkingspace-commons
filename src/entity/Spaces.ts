6ac68bb9b363d7fec57511d6f6711a3440430aec
Park Min Hyeok <opensource@pmh.codes>
2022-08-06 23:11:51 +0900

    feat: add Zone & Space entity

---
+++ src/entity/Spaces.ts
@@ -0,0 +1,63 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { Users } from './Users'
+import { Zones } from './Zones'
+
+/* eslint-disable no-unused-vars */
+
+export enum SpaceType {
+  MANUALLY,
+  AUTOMATICALLY
+}
+
+export enum SpaceStatus {
+  PENDING_APPROVE,
+  ENABLED,
+  DENIED,
+  DELETED
+}
+
+/* eslint-enable no-unused-vars */
+
+@Entity()
+export class Spaces {
+  @PrimaryGeneratedColumn({ name: 'spaces_id' })
+  public readonly id: number
+
+  @Column({ name: 'users_id' })
+  public readonly managerId: number
+
+  @ManyToOne(() => Users, (user) => user.id, { eager: true })
+  @JoinColumn({ name: 'users_id' })
+  public readonly manager: Users
+
+  @Column({ name: 'spaces_name' })
+  public readonly name: string
+
+  @Column({ name: 'spaces_description' })
+  public readonly description?: string
+
+  @Column({ name: 'spaces_lat' })
+  public readonly lat: number
+
+  @Column({ name: 'spaces_lng' })
+  public readonly lng: number
+
+  @Column({ name: 'spaces_default_cost' })
+  public readonly defaultCost: number
+
+  @Column({ name: 'spaces_type' })
+  public readonly type: SpaceType
+
+  @Column({ name: 'spaces_createdat', type: 'timestamp' })
+  public readonly createdAt: Date
+
+  @Column({ name: 'spaces_status' })
+  public readonly status: SpaceStatus
+
+  @Column({ name: 'spaces_unit' })
+  public readonly timeUnit: number
+
+  @OneToMany(() => Zones, (zone) => zone.spaceId)
+  @JoinColumn({ name: 'spaces_id' })
+  public readonly childrenZones: Zones[]
+}
---
+++ src/entity/Zones.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Spaces } from './Spaces'
+import { Users } from './Users'
+
+/* eslint-disable no-unused-vars */
+
+export enum ZoneStatus {
+  DISABLED,
+  ENABLED,
+  DELETED
+}
+
+/* eslint-enable no-unused-vars */
+
+@Entity()
+export class Zones {
+  @PrimaryGeneratedColumn({ name: 'zones_id' })
+  public readonly id: number
+
+  @Column({ name: 'spaces_id' })
+  public readonly spaceId: number
+
+  @ManyToOne(() => Spaces, (space) => space.id)
+  @JoinColumn({ name: 'spaces_id' })
+  public readonly parentSpace: Spaces
+
+  @Column({ name: 'users_id' })
+  public readonly managerId: number
+
+  @ManyToOne(() => Users, (user) => user.id)
+  @JoinColumn({ name: 'users_id' })
+  public readonly manager: Users
+
+  @Column({ name: 'zones_cost' })
+  public readonly costDiffrence?: number
+
+  @Column({ name: 'zones_status' })
+  public readonly status: ZoneStatus
+}
