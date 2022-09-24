7638dc63d407b95383272dceb532b29467676a4f
Taehyun Lim <admin@limtaehyun.dev>
2022-09-24 09:30:56 +0900

    feat: reviews entity

---
+++ src/entity/Review.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Spaces } from './Spaces'
+import { Users } from './Users'
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn({ name: 'reviews_id' })
+  public readonly id: number
+
+  @Column({ name: 'reviews_content' })
+  public readonly content: string
+
+  @Column({ name: 'reviews_rating' })
+  public readonly rating: number
+
+  @Column({ name: 'reviews_created_at' })
+  public readonly createdAt: Date
+
+  @Column({ name: 'reviews_deleted_at' })
+  public readonly deletedAt: Date
+
+  @Column({ name: 'users_id' })
+  public readonly writer: number
+
+  @Column({ name: 'space_id' })
+  public readonly spaceId: number
+
+  @ManyToOne(() => Users, (user) => user.id, { eager: true })
+  @JoinColumn({ name: 'users_id' })
+  public readonly user: Users
+
+  @ManyToOne(() => Spaces, (space) => space.id, { eager: true })
+  @JoinColumn({ name: 'space_id' })
+  public readonly space: Spaces
+}
